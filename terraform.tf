031543fece84fc0804303b6555f2388d58412261
raju-gautham <87421270+raju-gautham@users.noreply.github.com>
2021-07-16 21:40:17 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "terraformnew"
+  location = "Central India"
+}
+
